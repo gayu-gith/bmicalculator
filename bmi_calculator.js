5624e55037be0a91f676a23871746c241061d975
gayu-gith <gayugmoorthy2020@gmail.com>
2025-01-09 15:14:48 +0530

    Update bmi_calculator.js

--- bmi_calculator.js
+++ bmi_calculator.js
@@ -7,13 +7,12 @@ function calculateBmi() {
     document.getElementById('heading').innerHTML = '';
     return;
     }
-    // Convert height from cm to meters for the BMI formula
+    
     height = height / 100;
-    // BMI calculation formula for kg and meters
     let bmi = weight / (height * height);
     document.getElementById('heading').innerHTML = 'Your BMI is:'; 
     document.getElementById('bmi-output').innerHTML = bmi.toFixed(2);
-    // BMI classification
+    
     if (bmi < 18.5) { 
     document.getElementById('message').innerHTML = 'You are underweight'; 
     } else if (bmi >= 18.5 && bmi <= 24.9) { 
